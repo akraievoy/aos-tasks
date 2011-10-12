21a82e06bd456ce5326c0f2c0607655e26a0c8d6
akraievoy <akraievoy@gmail.com>
2011-10-12 23:40:06 +0300

    added TOCs, forced HTML to be a complete document

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pandoc -f markdown -t html -o aos-tasks.html aos-tasks.md
-pandoc -f markdown -t latex -o aos-tasks.latex --template=pandoc-latex-template.tex aos-tasks.md
+pandoc -sS --toc -f markdown -t html -o aos-tasks.html aos-tasks.md
+pandoc -sS --toc -f markdown -t latex -o aos-tasks.latex --template=pandoc-latex-template.tex aos-tasks.md
 pdflatex aos-tasks.latex
